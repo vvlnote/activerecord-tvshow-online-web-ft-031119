79ab198c5d7b185f97139225d1d0ddb26908e4fc
Joy Lin <vvlnote@hotmail.com>
2019-04-25 18:43:29 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+
+	def self.highest_rating
+
+	end
+
+	def self.most_popular_show
+
+	end
+
+	def self.lowest_rating
+
+	end
+
+	def self.least_popular_show
+
+	end
+
+	def self.ratings_sum
+
+	end
+
+	def self.popular_shows
+
+	end
+
+	def self.shows_by_alphabetical_order
+
+	end
+end
